08c474034008c896c21b7a7834ca67f3f61d0b67
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-06-17 16:29:48 -0600

    Add websocket listener

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -11,6 +11,7 @@ import {
 
 import Loading from './Loading';
 import Panel from './Panel';
+import { setInterview } from 'helpers/reducers';
 
 const data = [
   {
@@ -65,12 +66,26 @@ class Dashboard extends Component {
     });
 
     this.socket = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
+
+    this.socket.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+
+      if (typeof data === 'object' && data.type === 'SET_INTERVIEW') {
+        this.setState((previousState) =>
+          setInterview(previousState, data.id, data.interview)
+        );
+      }
+    };
   }
 
   componentDidUpdate(previousProps, previousState) {
     if (previousState.focused !== this.state.focused) {
       localStorage.setItem('focused', JSON.stringify(this.state.focused));
     }
+  }
+
+  componentWillUnmount() {
+    this.socket.close();
   }
 
   selectPanel(id) {
