5ec2ff3285e7a3b246736f8d1ebfe78fa5da05fe
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-06-17 15:17:10 -0600

    Add axios get request, add selectors to query data from api, add websocket variable

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-
 import classnames from 'classnames';
+
+import {
+  getTotalInterviews,
+  getLeastPopularTimeSlot,
+  getMostPopularDay,
+  getInterviewsPerDay,
+} from 'helpers/selectors';
 
 import Loading from './Loading';
 import Panel from './Panel';
@@ -10,22 +16,22 @@ const data = [
   {
     id: 1,
     label: 'Total Interviews',
-    value: 6,
+    getValue: getTotalInterviews,
   },
   {
     id: 2,
     label: 'Least Popular Time Slot',
-    value: '1pm',
+    getValue: getLeastPopularTimeSlot,
   },
   {
     id: 3,
     label: 'Most Popular Day',
-    value: 'Wednesday',
+    getValue: getMostPopularDay,
   },
   {
     id: 4,
     label: 'Interviews Per Day',
-    value: '2.3',
+    getValue: getInterviewsPerDay,
   },
 ];
 
@@ -44,6 +50,21 @@ class Dashboard extends Component {
     if (focused) {
       this.setState({ focused });
     }
+
+    Promise.all([
+      axios.get('/api/days'),
+      axios.get('/api/appointments'),
+      axios.get('/api/interviewers'),
+    ]).then(([days, appointments, interviewers]) => {
+      this.setState({
+        loading: false,
+        days: days.data,
+        appointments: appointments.data,
+        interviewers: interviewers.data,
+      });
+    });
+
+    this.socket = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
   }
 
   componentDidUpdate(previousProps, previousState) {
@@ -76,7 +97,7 @@ class Dashboard extends Component {
         <Panel
           key={panel.id}
           label={panel.label}
-          value={panel.value}
+          value={panel.getValue(this.state)}
           onSelect={(event) => this.selectPanel(panel.id)}
         />
       ));
